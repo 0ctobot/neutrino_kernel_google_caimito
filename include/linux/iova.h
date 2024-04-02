0351a660d2a7f54aa477eb7c1fe7c3a3dd671135
Sultan Alsawaf <sultan@kerneltoast.com>
2024-04-01 21:33:28 -0700

    zumapro_defconfig: Disable Android vendor/OEM data reservations in structs
    Now that nothing uses these reservations anymore, remove them since they
    are cruft.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- include/linux/iova.h
+++ include/linux/iova.h
@@ -40,7 +40,7 @@ struct iova_domain {
 	struct iova_rcache	*rcaches;
 	struct hlist_node	cpuhp_dead;
 
-#if IS_ENABLED(CONFIG_IOVAD_BEST_FIT_ALGO)
+#if IS_ENABLED(CONFIG_IOVAD_VENDOR_HOOKS)
 	u64		val;
 #endif
 
