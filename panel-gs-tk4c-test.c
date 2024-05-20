167e9fb107e62087da1a1dfd7368fa51557a3649
Safayat Ullah <safayat@google.com>
2024-05-20 13:00:46 +0000

    display: tk4c: dump important register values
    - IRC
    - Refresh rate
    - AOD status
    - FGZ
    - OPR
    
    Bug: 335744311
    Test: adb shell "cat /d/dri/0/DSI-1/panel/test/reg_values"
    Change-Id: Ib8f79711a4aec60d0593c6640e40709a0b8adfab
    Signed-off-by: Safayat Ullah <safayat@google.com>

--- panel-gs-tk4c-test.c
+++ panel-gs-tk4c-test.c
@@ -2,6 +2,51 @@
 
 #include "gs_panel/gs_panel_test.h"
 
+static const u8 test_key_enable[] = { 0xF0, 0x5A, 0x5A };
+static const u8 test_key_disable[] = { 0xF0, 0xA5, 0xA5 };
+
+const struct gs_dsi_cmd irc_read_cmds[] = {
+	GS_DSI_CMD(0xB0, 0x00, 0x03, 0x68),
+};
+static DEFINE_GS_CMDSET(irc_read);
+
+const struct gs_dsi_cmd fgz_read_cmds[] = {
+	GS_DSI_CMD(0xB0, 0x00, 0x61, 0x68),
+};
+static DEFINE_GS_CMDSET(fgz_read);
+
+const struct gs_dsi_cmd opr_read_cmds[] = {
+	GS_DSI_CMD(0xB0, 0x00, 0xD8, 0x63),
+};
+static DEFINE_GS_CMDSET(opr_read);
+
+const struct gs_dsi_cmd test_key_enable_cmds[] = {
+	GS_DSI_CMDLIST(test_key_enable),
+};
+static DEFINE_GS_CMDSET(test_key_enable);
+
+const struct gs_dsi_cmd test_key_disable_cmds[] = {
+	GS_DSI_CMDLIST(test_key_disable),
+};
+static DEFINE_GS_CMDSET(test_key_disable);
+
+/* sorted by register address */
+static const struct gs_panel_register tk4c_registers[] = {
+	GS_PANEL_REG("lp_status", 0x54),
+	GS_PANEL_REG_LONG_WITH_CMDS("opr", 0x63, 2, &opr_read_cmdset),
+	GS_PANEL_REG_WITH_CMDS("irc", 0x68, &irc_read_cmdset),
+	GS_PANEL_REG_LONG_WITH_CMDS("fgz", 0x68, 8, &fgz_read_cmdset),
+	GS_PANEL_REG_LONG("refresh_rate", 0x84, 2),
+};
+
+static const struct gs_panel_registers_desc tk4c_reg_desc = {
+	.register_count = ARRAY_SIZE(tk4c_registers),
+	.registers = tk4c_registers,
+
+	.global_pre_read_cmdset = &test_key_enable_cmdset,
+	.global_post_read_cmdset = &test_key_disable_cmdset,
+};
+
 /**
  * struct tk4c_panel_test - panel specific test runtime info
  *
@@ -27,6 +72,7 @@ static const struct gs_panel_test_funcs tk4c_test_func = {
 
 static const struct gs_panel_test_desc google_tk4c_test = {
 	.test_funcs = &tk4c_test_func,
+	.regs_desc = &tk4c_reg_desc,
 };
 
 static int tk4c_panel_test_probe(struct platform_device *pdev)
