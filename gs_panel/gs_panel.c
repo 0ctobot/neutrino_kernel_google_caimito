cf3879f5219255e8d5d94f5c89e149f164bcb6b2
Chris Lu <luchris@google.com>
2024-08-23 14:17:03 +0000

    gs_panel: add PID for panel trace
    Bug: 359422698
    Bug: 376330991
    Test: show logs in the same thread in the perfetto trace
    
    Signed-off-by: Chris Lu <luchris@google.com>
    Signed-off-by: Shiyong Li <shiyongli@google.com>
    (cherry picked from commit d0e577ca7341940c742948f2d8043da60dc7816e)
    (cherry picked from https://partner-android-review.googlesource.com/q/commit:e6bc436228f02a2bde03f8f491f4de839aa2d8c7)
    Merged-In: I088217ecf9fb30813b8f338fdfad315181ba9e4e
    Change-Id: I088217ecf9fb30813b8f338fdfad315181ba9e4e

--- gs_panel/gs_panel.c
+++ gs_panel/gs_panel.c
@@ -1484,6 +1484,7 @@ int gs_panel_first_enable_helper(struct gs_panel *ctx)
 	if (!ret)
 		ctx->initialized = true;
 
+	ctx->trace_pid = current->tgid;
 	if (!ctx->panel_rev) {
 		if (gs_panel_has_func(ctx, get_panel_rev)) {
 			u32 id;
